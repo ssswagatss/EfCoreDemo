4a213b0b1ef82203aa7b354fc129c5a2f132fa4c
Swagat Swain <ssswagatss@gmail.com>
2022-04-21 16:16:13 +0530

    Json serializer

--- EfCoreDemo/Program.cs
+++ EfCoreDemo/Program.cs
@@ -1,30 +1,38 @@
-﻿namespace EfCoreDemo
+﻿using Newtonsoft.Json;
+
+namespace EfCoreDemo
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            //var users = GetUsers();
+            //DisplayUsers(users);
+            var db = new EfDbContext();
+
             var users = GetUsers();
-            DisplayUsers(users);
+            db.Users.AddRange(users);
+            db.SaveChanges();
+
+            Console.Clear();
+            DisplayUsers(db.Users.ToList());
             Console.ReadKey();
         }
 
         private static void DisplayUsers(List<User> users)
         {
-            foreach (var u in users)
-            {
-                Console.WriteLine($"Name-{u.LastName} {u.LastName} | Age-{u.Age} | UserName-{u.UserName}");
-            }
+            
+            Console.WriteLine(JsonConvert.SerializeObject(users, Formatting.Indented));
         }
 
         private static List<User> GetUsers()
         {
             var users = new List<User>();
-            users.Add(new User { UserId = 1, UserName = "ssswagatss", Age = 30, FirstName = "Swagat", LastName = "Swain" });
-            users.Add(new User { UserId = 2, UserName = "sonalis", Age = 25, FirstName = "Sonali", LastName = "Sahoo" });
-            users.Add(new User { UserId = 2, UserName = "bijayam", Age = 28, FirstName = "Bijaya", LastName = "Mishra" });
-            users.Add(new User { UserId = 2, UserName = "naibedyak", Age = 35, FirstName = "Naibedya", LastName = "Kar" });
+            users.Add(new User { UserName = "ssswagatss", Age = 30, FirstName = "Swagat", LastName = "Swain" });
+            users.Add(new User { UserName = "sonalis", Age = 25, FirstName = "Sonali", LastName = "Sahoo" });
+            users.Add(new User { UserName = "bijayam", Age = 28, FirstName = "Bijaya", LastName = "Mishra" });
+            users.Add(new User { UserName = "naibedyak", Age = 35, FirstName = "Naibedya", LastName = "Kar" });
             return users;
         }
     }
